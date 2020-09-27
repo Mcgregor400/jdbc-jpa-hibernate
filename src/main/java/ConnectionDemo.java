71b64d4a6119f744f19e17a75c4e9eee0347f1f8
Mcgregor400 <60290742+Mcgregor400@users.noreply.github.com>
2020-09-27 17:33:36 +0200

    Niedziela 17 09 test

--- src/main/java/ConnectionDemo.java
+++ src/main/java/ConnectionDemo.java
@@ -4,17 +4,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException, InstantiationException, SQLException
+    {
         Connection connection = getConnection();
         connection.close();
     }
 
-    public static Connection getConnection() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/java6?serverTimezone=Europe/Warsaw",
-                "root",
-                "1234");
-        return connection;
-    }
+//    public static Connection getConnection() throws InstantiationException, IllegalAccessException,
+//            InvocationTargetException, NoSuchMethodException, ClassNotFoundException, SQLException
+//    {
+//        Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+//        Connection connection = DriverManager.getConnection(
+//                "jdbc:mysql://localhost:3306/java6?serverTimezone=Europe/Warsaw",
+//                "root",
+//                "1234");
+//        return connection;
+//    }
 }
